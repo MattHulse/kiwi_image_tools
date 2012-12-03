f0411a9e9d75e5d3c62b1b91a6788f2a20dfc0f4
Matt Hulse <matt.hulse@gmail.com>
2012-12-03 00:46:30 -0700

    Update line spacing tests

--- test/test_line_spacing.rb
+++ test/test_line_spacing.rb
@@ -45,13 +45,13 @@ g = KiwiImageTools::Generator.new(
                   :size => 18},
     :number => {:text => "Nbr 103452",
                 :color => 'black',
-                :size => 15},
+                :size => 18},
     :contact => {:text => "23 K-road, Newton, Auckland.  Phone 09-8886767\nor at another address\nyour choice",
                 :color => 'black',
-                :size => 15},
+                :size => 18},
     :conditions => {:text => "One vote per person.\nVoid where prohibited.\nValid at participating locations only",
                 :color => 'red',
-                :size => 15}
+                :size => 18}
 )
 
 g.display_image
@@ -73,13 +73,13 @@ g = KiwiImageTools::Generator.new(
                   :size => 18},
     :number => {:text => "Nbr 103452",
                 :color => 'black',
-                :size => 15},
+                :size => 18},
     :contact => {:text => "23 K-road, Newton, Auckland.  Phone 09-8886767",
                 :color => 'black',
-                :size => 15},
+                :size => 18},
     :conditions => {:text => "One vote per person.\nVoid where prohibited.\nValid at participating locations only",
                 :color => 'red',
-                :size => 15}
+                :size => 18}
 )
 
 g.display_image
